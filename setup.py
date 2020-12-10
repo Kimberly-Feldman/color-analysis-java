5077d227f357c4a36f33e717b0fd496707539192
Kim <kimberly.feldman@rutgers.edu>
2020-12-10 00:16:24 -0500

    created library

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='coloranalysis',
     packages=find_packages(include=['src/coloranalysis']),
     version='0.1.0',
-    description="Retrieves color palette from imgix hosted image, suggests font color"
+    description="Retrieves color palette from imgix hosted image, suggests font color",
     author='Kim',
     license='MIT',
 )
